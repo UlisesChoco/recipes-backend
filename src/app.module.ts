15f980f8f11256425b31a9a03416acbf66fe580f
UlisesChoco <ulisesjustosaucedo@gmail.com>
2026-04-17 16:59:57 +0000

    [REC-16] Add TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './configuration/database.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.name'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
