6022e08c96f545e9771575e0e64193a38617bd7c
UlisesChoco <ulisesjustosaucedo@gmail.com>
2026-04-17 17:01:19 +0000

    [REC-13] Create User controller

---
+++ src/user/controller/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Query } from "@nestjs/common";
+import { UserService } from "../service/user.service";
+
+@Controller('users')
+export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ) { }
+
+    @Get()
+    async getByEmail(
+        @Query("email") email: string
+    ) {
+        return await this.userService.findByEmail(email);
+    }
+}
